de6ad04f5274e8cbb18c90d975a6dfcd74c104be
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2014-07-22 13:17:23 -0400

    Enhance supplemental/flush_tyt.sh for my OpenPandora
    - Actually resolve the device node from $FLASH
    - Just call op_usbhost.sh directly
    - Use op_cpuspeed.sh to remove two more manual steps from the process
    
    (That just leaves populating the playlist, clicking Play, and emptying it)

--- supplemental/flush_tyt.sh
+++ supplemental/flush_tyt.sh
@@ -1,14 +1,17 @@
 #/bin/bash
-FLASH=/media/16GB_Flash
-devnode=$(grep /media/16 /proc/mounts | awk '{ print $1 }')
+export FLASH=/media/16GB_Flash
+devnode=$(grep "$FLASH" /proc/mounts | awk '{ print $1 }')
 
 if [ -e "$FLASH" ]; then
 	killall smplayer2
 	rm -f "$FLASH"/*.{mp4,webm}
 	exo-mount -de "$devnode"
-	/usr/pandora/scripts/pnd_run.sh -p "/usr/pandora/apps//op_usbhost.pnd" -e "op_usbhost.sh" -b "op_usbhost"
+	sudo -n /usr/pandora/scripts/op_usbhost.sh
+	sudo -n /usr/pandora/scripts/op_cpuspeed.sh -n 600
 	notify-send 'You may now remove the thumbdrive'
+
 else
-	/usr/pandora/scripts/pnd_run.sh -p "/usr/pandora/apps//op_usbhost.pnd" -e "op_usbhost.sh" -b "op_usbhost"
+	sudo -n /usr/pandora/scripts/op_cpuspeed.sh -n 800
+	sudo -n /usr/pandora/scripts/op_usbhost.sh
 	/usr/bin/nohup /usr/pandora/scripts/pnd_run.sh -p "/media/OS_32GB/pandora/menu//smplayer2_r6.pnd" -e "smplayer2.sh" -b "smplayer2"
 fi
