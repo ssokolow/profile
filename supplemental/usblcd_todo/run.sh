4592c59670206063893f5ddb3baa1f2fed2572fc
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2015-08-22 00:05:56 -0400

    Hack together quick way to display my TODO list on LCDSysInfo USB device

---
+++ supplemental/usblcd_todo/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+./todo_list.py /home/ssokolow/Desktop/TODO_TODAY.txt &
---
+++ supplemental/usblcd_todo/todo_list.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""TODO list driver for LCDSysInfo device"""
+
+from __future__ import (absolute_import, division, print_function,
+                        with_statement, unicode_literals)
+
+__author__ = "Stephan Sokolow (deitarion/SSokolow)"
+__appname__ = "[application name here]"
+__version__ = "0.0pre0"
+__license__ = "GNU GPL 3.0 or later"
+
+import logging, os, time
+
+import yaml
+from pylcdsysinfo import (BackgroundColours, LCDSysInfo, TextAlignment,
+                          TextColours, TextLines)
+import pyinotify
+
+BRIGHTNESS = 48
+BGCOLOR = BackgroundColours.BLACK
+FGCOLOR = TextColours.GREY
+log = logging.getLogger(__name__)
+
+def monitor_file(path):
+    class EventHandler(pyinotify.ProcessEvent):
+        last_mtime = 0
+
+        @staticmethod
+        def fmt_task(task):
+            if task.startswith('+'):
+                task = '+ ' + task[1:]
+            else:
+                task = '- ' + task
+            return '____%s' % task
+
+        def process_IN_MODIFY(self, event):
+            # Hack around a race condition
+            time.sleep(1)
+
+            # Ensure we fire only once per change
+            this_mtime = os.stat(path).st_mtime
+            if self.last_mtime == this_mtime:
+                return
+            else:
+                self.last_mtime = this_mtime
+
+            try:
+                with open(path, 'rU') as fobj:
+                    yobj = yaml.safe_load_all(fobj)
+                    yobj.next()  # Skip header text
+                    data = yobj.next()
+            except StopIteration:
+                log.error("Couldn't parse data from file: %s", path)
+                return  # Don't die on a bad file
+
+            tasks = data.get('TODO')
+            if not tasks:
+                return
+
+            lcd = LCDSysInfo()
+            lcd.set_brightness(BRIGHTNESS)
+            lcd.dim_when_idle(False)
+            lcd.clear_lines(TextLines.ALL, BGCOLOR)
+
+            lines = ["TODO:"] + [self.fmt_task(x) for x in tasks]
+            for pos, line in enumerate(lines[:6]):
+                lcd.display_text_on_line(pos + 1, line, False,
+                                         TextAlignment.LEFT, FGCOLOR)
+
+    handler = EventHandler()
+    handler.process_IN_MODIFY(None)
+
+    wm = pyinotify.WatchManager()
+    wdd = wm.add_watch(path, pyinotify.IN_MODIFY)
+    notifier = pyinotify.Notifier(wm, handler)
+    notifier.loop()
+
+def main():
+    """The main entry point, compatible with setuptools entry points."""
+    from argparse import ArgumentParser
+    parser = ArgumentParser(usage="%(prog)s [options] <argument> ...",
+            description=__doc__.replace('\r\n', '\n').split('\n--snip--\n')[0])
+    parser.add_argument('--version', action='version',
+            version="%%(prog)s v%s" % __version__)
+    parser.add_argument('-v', '--verbose', action="count", dest="verbose",
+        default=2, help="Increase the verbosity. Use twice for extra effect")
+    parser.add_argument('-q', '--quiet', action="count", dest="quiet",
+        default=0, help="Decrease the verbosity. Use twice for extra effect")
+    parser.add_argument('path', action="store")
+    # Reminder: %(default)s can be used in help strings.
+
+    args = parser.parse_args()
+
+    # Set up clean logging to stderr
+    log_levels = [logging.CRITICAL, logging.ERROR, logging.WARNING,
+                  logging.INFO, logging.DEBUG]
+    args.verbose = min(args.verbose - args.quiet, len(log_levels) - 1)
+    args.verbose = max(args.verbose, 0)
+    logging.basicConfig(level=log_levels[args.verbose],
+                        format='%(levelname)s: %(message)s')
+
+    monitor_file(args.path)
+
+if __name__ == '__main__':
+    main()
+
+# vim: set sw=4 sts=4 expandtab :
