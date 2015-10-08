186a906aaa1b9b595fd238f4447ea48e0a927522
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2015-10-07 21:28:47 -0400

    More enhancements to my USBLCD TODO list.

--- supplemental/usblcd_todo/run.sh
+++ supplemental/usblcd_todo/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-./todo_list.py /home/ssokolow/Desktop/TODO_TODAY.txt "$@" &
+./todo_list.py /home/ssokolow/Desktop/TODO_TODAY.txt "$@" 2>&1 | tee -a /tmp/todo_list.log &
