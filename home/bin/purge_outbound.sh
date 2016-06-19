26d3a35bd54c6b1dd9a6815eed84584e14264090
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2016-06-19 00:24:38 -0400

    Fix purge_outbound.sh now that I've tested with actual stale files.

--- home/bin/purge_outbound.sh
+++ home/bin/purge_outbound.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for X in /srv/inbound/FOR_{ANDRE,MOM,NICK} /srv/inbound/Stuff\ to\ Print; do
-	find "$X" -ctime +30 -exec echo rm -rf {} \+ 2>&1 | grep -v 'No such file or directory'
+	find "$X" -mindepth 1 -ctime +30 -exec rm -rf {} \+ 2>&1 | grep -v 'No such file or directory'
 done
