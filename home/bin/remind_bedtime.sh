63834628929bd7007b1dd3936d69f9d97c204515
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2017-08-26 11:15:37 -0400

    Fix a "testing is not representative" bug in remind_bedtime

--- home/bin/remind_bedtime.sh
+++ home/bin/remind_bedtime.sh
@@ -2,7 +2,6 @@
 
 # Edit and kill the program to change bedtime
 while true; do
-    # Ensure we get the Xfce notification daemon under KDE
-    DBUS_SESSION_BUS_ADDRESS='' ~/bin/remind_bedtime
+    ~/bin/remind_bedtime
     sleep 1
 done
