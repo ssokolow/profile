790a28a3c2e416bdb5f78009aa11c6c59d4aa9c1
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2021-02-13 19:18:26 -0500

    Document what fix_gtk3.sh does

--- home/.kde/env/fix_gtk3.sh
+++ home/.kde/env/fix_gtk3.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+# Workaround for GTK+ 3.x bug
+# Source:
+#   https://bugs.launchpad.net/ubuntu/+source/gtk+3.0/+bug/1240957
+#   https://bugs.kde.org/show_bug.cgi?id=348270
 export GDK_CORE_DEVICE_EVENTS=1
